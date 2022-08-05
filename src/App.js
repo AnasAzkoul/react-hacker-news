4a3598ab66a2efe102331c1eede5c4a574db84b1
Anas azkoul <anas@Anass-MacBook-Air.local>
2022-08-05 11:33:24 +0200

    main setup and initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchForm from './SearchForm';
+import Stories from './Stories';
+import Buttons from './Buttons';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h2>hacker news starter</h2>;
 }
 
 export default App;
